8c0a3b3d854a9cbf009d8a4a3073d23d85ed515a
adetech2017 <adeyemi879@gmail.com>
2026-05-12 15:24:40 +0100

    include Candidate Portrait

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -38,13 +38,8 @@
               </li>
             </ul>
           </div>
-          <div class="h-96 bg-light-gray rounded-lg flex items-center justify-center text-gray-400">
-            <div class="text-center">
-              <svg class="w-20 h-20 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
-              <p>Candidate Portrait</p>
-            </div>
+          <div class="h-96 bg-light-gray rounded-lg overflow-hidden flex items-center justify-center text-gray-400">
+            <img :src="portraitImage" alt="Kadri Obafemi Hamzat" class="w-full h-full object-cover" />
           </div>
         </div>
       </div>
@@ -205,6 +200,8 @@ import TestimonialCard from '@/Components/TestimonialCard.vue'
 import SocialFeedsSection from '@/Components/SocialFeedsSection.vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Autoplay } from 'swiper/modules'
+
+const portraitImage = '/storage/personal/koh2.jpg'
 
 defineProps({
   heroSlides: Array,
